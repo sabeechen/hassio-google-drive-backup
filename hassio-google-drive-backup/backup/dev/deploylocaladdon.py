2178b47ed753ba3ec7030317233271db82c1d27b
sabeechen <stephen@beechens.com>
2020-02-08 17:28:23 +0000

    Add a script to deploy an addon locally

---
+++ hassio-google-drive-backup/backup/dev/deploylocaladdon.py
@@ -0,0 +1,75 @@
+import os
+import requests
+import shutil
+import json
+import time
+import pprint
+from os.path import join, abspath
+
+
+def main():
+    installed = get("supervisor/info")["addons"]
+    is_installed = False
+    for addon in installed:
+        if addon['slug'] == "local_hassio_google_drive_backup" and addon['state'] != 'stopped':
+            # Stop the addon
+            print("Stopping the old addon")
+            post("addons/local_hassio_google_drive_backup/stop")
+            is_installed = True
+
+    # install the addon
+    if os.path.exists("/addons/hassio-google-drive-backup"):
+        print("Deleting old files")
+        shutil.rmtree("/addons/hassio-google-drive-backup")
+
+    # copy in addon files
+    src = abspath(join(__file__, "..", "..", ".."))
+    print("Copying new files")
+    shutil.copytree(src, "/addons/hassio-google-drive-backup")
+
+    # remove the image file
+    with open("/addons/hassio-google-drive-backup/config.json") as f:
+        config = json.load(f)
+    del config['image']
+    with open("/addons/hassio-google-drive-backup/config.json", "w+") as f:
+        json.dump(config, f)
+
+    post("addons/reload")
+    if not is_installed:
+        print("Installing docker image (this can take a few minutes)")
+        post("addons/local_hassio_google_drive_backup/install")
+    else:
+        print("Rebuilding docker image (this can take a few minutes)")
+        post("addons/local_hassio_google_drive_backup/rebuild")
+
+    time.sleep(5)
+    print("Starting addon")
+    post("addons/local_hassio_google_drive_backup/start")
+
+
+def get(endpoint, json=True):
+    headers = {
+        "X-HASSIO-KEY": os.environ.get('HASSIO_TOKEN')
+    }
+    resp = requests.get("http://hassio/" + endpoint, headers=headers)
+    resp.raise_for_status()
+    if json:
+        return resp.json()['data']
+    else:
+        return resp.text
+
+
+def post(endpoint, json=True):
+    headers = {
+        "X-HASSIO-KEY": os.environ.get('HASSIO_TOKEN')
+    }
+    resp = requests.post("http://hassio/" + endpoint, headers=headers)
+    resp.raise_for_status()
+    if json:
+        return resp.json()['data']
+    else:
+        return resp.text
+
+
+if __name__ == '__main__':
+    main()

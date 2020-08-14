6963d187295269e74509def1d4502d2d209dc405
Stephen <stephen@beechens.com>
2020-08-14 10:53:54 -0600

    Build the addon locally (cloudbuild can't do arm or something)

--- hassio-google-drive-backup/dev/deploy_addon.py
+++ hassio-google-drive-backup/dev/deploy_addon.py
@@ -1,15 +1,21 @@
 import getpass
 import subprocess
 import os
+import json
+from os.path import abspath, join
 
-try:
-    p = getpass.getpass("Enter DockerHub Password")
-except Exception as error:
-    print('ERROR', error)
-    exit()
+with open(abspath(join(__file__, "..", "..", "config.json"))) as f:
+    version = json.load(f)["version"]
+print("Version will be: " + version)
+subprocess.run("docker login", shell=True)
+
+
+platforms = ["amd64", "armv7", "aarch64", "armhf", "i386"]
 
 os.chdir("hassio-google-drive-backup")
-print("Setting the appropriate gcloud project...")
-subprocess.run("gcloud config set project hassio-drive-backup", shell=True)
-print("Building and uploading addon containers container...")
-subprocess.run("gcloud builds submit --config cloudbuild-addon.yaml --substitutions _DOCKERHUB_PASSWORD=" + p + ",_VERSION=" + version, shell=True)
+for platform in platforms:
+    subprocess.run("docker build -f Dockerfile-addon -t sabeechen/hassio-google-drive-backup-{0}:{1} --build-arg BUILD_FROM=homeassistant/{0}-base .".format(platform, version), shell=True)
+
+for platform in platforms:
+    subprocess.run("docker push sabeechen/hassio-google-drive-backup-{0}:{1}".format(platform, version), shell=True)
+

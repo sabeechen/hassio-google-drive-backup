ee222f7fe55a0b32aa751ef0dbdbaa9f19e1995e
Stephen <stephen@beechens.com>
2020-09-14 01:20:40 -0600

    Add tests for the version parser

---
+++ hassio-google-drive-backup/tests/test_version.py
@@ -0,0 +1,27 @@
+from backup.config import Version
+
+
+def test_default():
+    assert Version.default() == Version.default()
+    assert not Version.default() > Version.default()
+    assert not Version.default() < Version.default()
+    assert not Version.default() != Version.default()
+    assert Version.default() >= Version.default()
+    assert Version.default() <= Version.default()
+
+
+def test_version():
+    assert Version(1, 2, 3) == Version(1, 2, 3)
+    assert Version(1, 2, 3) >= Version(1, 2, 3)
+    assert Version(1, 2, 3) <= Version(1, 2, 3)
+    assert Version(1, 2, 3) > Version(1, 2)
+    assert Version(1) < Version(2)
+    assert Version(2) > Version(1)
+    assert Version(1) != Version(2)
+    assert Version(1, 2) > Version(1)
+    assert Version(1) < Version(1, 2)
+
+
+def test_parse():
+    assert Version.parse("1.0") == Version(1, 0)
+    assert Version.parse("1.2.3") == Version(1, 2, 3)

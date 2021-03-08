0953693abd87bbb52346108361886f80277ac61c
Stephen <stephen@beechens.com>
2021-03-08 13:39:28 -0700

    Sexy up the about page

--- hassio-google-drive-backup/backup/static/js/about.js
+++ hassio-google-drive-backup/backup/static/js/about.js
@@ -1,29 +1,78 @@
+var contributorsLoaded = false;
 async function fetchContributors() {
   const data = await fetch("https://api.github.com/repos/sabeechen/hassio-google-drive-backup/contributors");
-  const contributorsCountToShow = 11;
+  const contributorsCountToShow = 14;
 
-  const contributors = await data.json();
-  const container = document.getElementById("contributors");
-  container.innerHTML = "";
+  if (data.ok) {
+    const contributors = await data.json();
+    const containerSimple = document.getElementById("contributors-simple");
+    const containerExtra = document.getElementById("contributors-extra");
+    containerSimple.innerHTML = "";
+    containerExtra.innerHTML = "";
+    extra_info = {
+      'sabeechen': 'Original author.  Did most of this.',
+      'ericmatte': 'Redesigned the UI with divine HTML and CSS wizardry.',
+      'jhampson-dbre': "Fixed a really nasty timing bug." 
+    };
+    contributors.slice(0, contributorsCountToShow).forEach((contributor) => {
+      if (contributor.login in extra_info) {
+        const img = document.createElement("img");
+        img.src = contributor.avatar_url;
+        img.className = "contributor-img";
+        img.title = `@${contributor.login}`;
+        img.alt = img.title;
 
-  contributors.slice(0, contributorsCountToShow).forEach((contributor) => {
-    const img = document.createElement("img");
-    img.src = contributor.avatar_url;
-    img.className = "contributor-img";
-    img.title = `@${contributor.login}`;
-    img.alt = img.title;
+        const a = document.createElement("a");
+        a.href = `https://github.com/${contributor.login}`;
+        a.target = "_blank";
+        a.appendChild(img);
 
-    const a = document.createElement("a");
-    a.href = contributor.avatar_url;
-    a.appendChild(img);
-    container.appendChild(a);
-  });
+        const name = document.createElement("span");
+        name.innerHTML = `@${contributor.login}<br>${contributor.contributions} contributions`;
+        name.classList.add('contributor-name');
+        name.classList.add('center')
+        a.appendChild(name);
 
-  document.getElementById("contributors-more-link-count").innerHTML = contributors.length - contributorsCountToShow;
-  document.getElementById("contributors-count").innerHTML = contributors.length;
+        const div = document.createElement("div");
+        div.appendChild(a);
+
+        const detail = document.createElement("span");
+        detail.innerHTML = extra_info[contributor.login];
+        div.append(detail);
+        containerExtra.appendChild(div);
+        containerExtra.appendChild(document.createElement("br"));
+      } else {
+        const img = document.createElement("img");
+        img.src = contributor.avatar_url;
+        img.className = "contributor-img";
+        img.title = `@${contributor.login}`;
+        img.alt = img.title;
+
+        const a = document.createElement("a");
+        a.href = `https://github.com/${contributor.login}`;
+        a.target = "_blank";
+        a.appendChild(img);
+        containerSimple.appendChild(a);
+      }
+    });
+
+    document.getElementById("contributors-more-link-count").innerHTML = contributors.length - contributorsCountToShow;
+    document.getElementById("contributors-count").innerHTML = contributors.length;
+    document.getElementById("contributors-count").classList.remove('default-hidden');
+    document.getElementById("contributors-loading").classList.add('default-hidden');
+  } else {
+    document.getElementById("contributors-loading").classList.add('default-hidden');
+  }
 }
 
 $(document).ready(function () {
-  fetchContributors();
   M.Modal.init(document.querySelector("#about_modal"));
 });
+
+function openAbout() {
+  if (!contributorsLoaded) {
+    fetchContributors();
+    contributorsLoaded = true;
+  }
+  M.Modal.getInstance(document.getElementById("about_modal")).open();
+}

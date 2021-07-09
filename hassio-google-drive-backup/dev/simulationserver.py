659ef3d313cd022bb990f83deabbde9319e5f360
Stephen Beechen <stephen@beechens.com>
2021-07-09 16:28:02 -0600

    Fix simulated server auth

--- hassio-google-drive-backup/dev/simulationserver.py
+++ hassio-google-drive-backup/dev/simulationserver.py
@@ -1,6 +1,5 @@
 import re
 from typing import Dict
-from aiohttp.helpers import TOKEN
 from yarl import URL
 import aiohttp
 from aiohttp.web import (Application,
@@ -132,7 +131,7 @@ class SimServerModule(BaseModule):
     def getConfig(self) -> Config:
         return Config.withOverrides({
             Setting.DRIVE_AUTHORIZE_URL: str(self._base_url.with_path("o/oauth2/v2/auth")),
-            Setting.TOKEN_SERVER_HOST: self._base_url.host,
+            Setting.TOKEN_SERVER_HOST: str(self._base_url),
             Setting.DRIVE_TOKEN_URL: str(self._base_url.with_path("token")),
             Setting.DRIVE_REFRESH_URL: str(self._base_url.with_path("oauth2/v4/token")),
             Setting.INGRESS_PORT: 56152

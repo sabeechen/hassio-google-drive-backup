e0edfaa12ddeb9033a456a644ea87eaf6e814985
Stephen Beechen <stephen@beechens.com>
2022-03-20 17:23:34 -0600

    Ignore modules check

--- hassio-google-drive-backup/backup/debug/debug_server.py
+++ hassio-google-drive-backup/backup/debug/debug_server.py
@@ -1,6 +1,5 @@
 from backup.config import Config, Setting, Startable
 from backup.logger import getLogger
-import sys
 from injector import inject, singleton
 logger = getLogger(__name__)
 
@@ -13,10 +12,7 @@ class DebugServer(Startable):
 
     async def start(self):
         if self._config.get(Setting.DEBUGGER_PORT) is not None:
-            if 'debugpy' not in sys.modules:
-                logger.error("Unable to start the debugger server because the debugpy library is not installed")
-            else:
-                import debugpy
-                port = self._config.get(Setting.DEBUGGER_PORT)
-                logger.info("Starting debugger on port {}".format(port))
-                debugpy.listen(("0.0.0.0", port))
+            import debugpy
+            port = self._config.get(Setting.DEBUGGER_PORT)
+            logger.info("Starting debugger on port {}".format(port))
+            debugpy.listen(("0.0.0.0", port))
